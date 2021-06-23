e5687a8a143b2538587a0da790353d2dd381c590
Takumi Crary <tcrary@gmail.com>
2021-06-23 23:36:06 +0900

    Refactor fbm a little bit

--- app/fbm.hs
+++ app/fbm.hs
@@ -8,9 +8,6 @@ import SandboxCanvasBoilerplate
 
 main = printGLSLSandbox $ toProgram color
 
-color :: Vec4
-color = vec4 (0,0,0,0)
-
 dot :: Vec2 -> Vec2 -> Vec1
 dot = op2pre' "dot"
 
@@ -30,15 +27,17 @@ noise pos = (mix (x_ u) a b) + (c - a) * (y_ u) * (1.0 - x_ u) + (d - b) * (x_ u
         d = mrandom $ i + vec2 (1.0, 1.0)
         u = f * f * (3.0 - 2.0 * f)
 
-num_octaves = 16 --TODO
-
-fbm :: Vec2 -> Vec1
-fbm pos = v
+--TODO Expose monoToProgram and test these functions indvidiually
+fbm :: Vec2 -> Int -> Vec1
+fbm pos num_octaves = v
     where a = 0.5
           vinit = 0.5
           shift = vec2(100.0, 100.0) + mouse
           rot = mat22 ((cos 0.1) , (sin 0.5), (- sin 0.5), (cos 0.5))
-          --TODO finish fbm and the main
           fbmOctave (v,pos,a) = (v + a * noise pos, mul rot (pos * 2.0 + shift + mouse), a * 0.5)
           xs = iterate fbmOctave (vinit, pos, a)
           (v, _, _) = xs !! num_octaves
+
+
+color :: Vec4
+color = undefined --TODO!!
