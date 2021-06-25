c970c1c8fb7f77fb7414ca2f1e2d6088db95e671
Takumi Crary <tcrary@gmail.com>
2021-06-25 07:33:46 +0100

    Finalize working version of Lines
    http://glslsandbox.com/e#73605.1

--- app/Lines.hs
+++ app/Lines.hs
@@ -5,7 +5,15 @@ import Hylogen.Expr
 
 import SandboxCanvasBoilerplate
 
-main = printGLSLCanvas output
+
+toTestFunction = monoToProgram . toMono
+
+position = (((xy_ gl_FragCoord) * 2.0) - resolution) / (vec2 (mrez, mrez))
+mrez = (min_ (x_ resolution) (y_ resolution))
+destColor = vec3(0.0, 0.25, 1.0)
+
+-- ORIGINAL REFERENCE: http://glslsandbox.com/e#73582.0
+main = printGLSLSandbox output
 --TODO we need some sort of way to look through the Hylogen AST and replace uniforms
 
 output :: Program
@@ -16,15 +24,10 @@ num_lines = 10
 color :: Vec4
 color = vec4 (destColor * destForm, 1.0)
     where
-        position = (((xy_ gl_FragCoord) * 2.0) - resolution) / (vec2 (rhs, rhs))
-        rhs = (min_ (x_ resolution) (y_ resolution))
-        destColor = vec3(0.0, 0.25, 1.0)
-
+        xs = iterate destFormIter (vec3 (0.0,0.0,0.0), y_ position, 0.0)
         destFormIter (destForm, ypos, i) = (destForm + vec3 (l,l,l), q, i + 1)
             where
-                j = i + 0.0
-                ypos_update = (sin ((x_ position) * 2.0 + time * 1.2 * (cos (j * 1.314)) * 0.05))
-                q = ypos + ypos_update
-                l = 0.0035 / abs q
-        xs = iterate destFormIter (vec3 (0.0,0.0,0.0), y_ position, 0.0)
+                q = ypos + ( 0.05 * (sin ((x_ position) * 2.0 + time * 1.2 * (cos (i * 1.314)))))
+                l = 0.0035 / (abs q)
+
         (destForm, _, _) = xs !! num_lines
