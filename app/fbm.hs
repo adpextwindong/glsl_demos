a334d02a2f5e4a6f81eaee365deda3a15e89f5fe
Takumi Crary <tcrary@gmail.com>
2021-06-26 18:38:54 +0900

    Fix noise function after finding a spec mismatch in Hylogen's Global.hs

--- app/fbm.hs
+++ app/fbm.hs
@@ -11,18 +11,27 @@ dot :: Vec2 -> Vec2 -> Vec1
 dot = op2pre' "dot"
 
 mrandom :: Vec2 -> Vec1
-mrandom pos = sin (dot (xy_ pos) (vec2 (12.9898, 78.233))) * 43758.8453123
+mrandom pos = fract (c * sin dt)
+    where
+        a = 12.9898
+        b = 78.233
+        c =  43758.844 --NOTE BE CAREFUL OF TRUNCATION/ROUNDING? TODO LOOK AT THE SHOW INSTANCE
+        --c =  43758.5453123
+        v = vec2 (a, b)
+        dt = dot pos v :: Vec1
 
 noise :: Vec2 -> Vec1
-noise pos = (mix (x_ u) a b) + (c - a) * (y_ u) * (1.0 - x_ u) + (d - b) * (x_ u) * (y_ u)
+noise pos = mix a b (x_ u) +
+            ((c - a) * (y_ u) * (1.0 - x_ u)) +
+            ((d - b) * (x_ u) * (y_ u))
     where
-        i = floor_ pos
-        f = fract pos
-        a = mrandom $ i + vec2 (0.0, 0.0)
-        b = mrandom $ i + vec2 (1.0, 0.0)
-        c = mrandom $ i + vec2 (0.0, 1.0)
-        d = mrandom $ i + vec2 (1.0, 1.0)
-        u = f * f * (3.0 - 2.0 * f)
+        i = floor_ pos :: Vec2
+        f = fract pos :: Vec2
+        a = mrandom $ (i + vec2 (0.0, 0.0)) :: Vec1
+        b = mrandom $ (i + vec2 (1.0, 0.0)) :: Vec1
+        c = mrandom $ (i + vec2 (0.0, 1.0))
+        d = mrandom $ (i + vec2 (1.0, 1.0))
+        u = f * f * (3.0 - (2.0 * f))
 
 --TODO Expose monoToProgram and test these functions indvidiually
 fbm :: Int -> Vec2 -> Vec1
@@ -50,12 +59,14 @@ r = vec2 ( fbm octaves (p + 1.0 * q + vec2 (8.3, 2.8) + vec2 (0.15 * time2, 0.15
 
 f = fbm 16 (p + r)
 --TODO UNLIFT THE CONTENT FOR THIS FUNCTION
+
 color :: Vec4
 --color = vec4 (x_ end_color, y_ end_color, z_ end_color, 1.0)
-color = vec4 (noise (p + 1.0),
-             (noise (p + 1.0 * q + vec2 (8.3, 2.8) + vec2 (0.15 * time2, 0.15 * time2))),
-             (noise (p + 1.0 * q + vec2 (8.3, 2.8) + vec2 (0.126 * time2, 0.126 * time2))),
+color = vec4 (noise p,
+               0.0,
+               0.0,
                1.0)
+
 
 --Ideally vec2 / vec1 should work but the fractional instance forces a -> a -> a
 --Making it expect a vec1 on the right side of the /
@@ -63,14 +74,14 @@ color = vec4 (noise (p + 1.0),
 
 
 t1 = clamp ((f * f) * 4.0) (0.0 :: Vec1) (1.0 :: Vec1)
-c1 = mix t1 (vec3 (-10, -10, 2)) (vec3 (0.3667, 15939, 0.367) * (vec3 (x_ mouse, y_ mouse, 1.0)))
+--c1 = mix (vec3 (-10, -10, 2)) (vec3 (0.3667, 15939, 0.367) * (vec3 (x_ mouse, y_ mouse, 1.0)))
 
 t2 = clamp (len q) 0.1 5.0
-c2 = mix t2 (vec3 (-10, -10, 0.5)) c1
+--c2 = mix t2 (vec3 (-10, -10, 0.5)) c1
 --TODO double check mix ordering of arguments compared to original
 
 t3 = clamp (len (x_ r)) 0.0 1.0
-c3 = mix t3 (vec3 (1, 1, 0.5)) c2
+--c3 = mix t3 (vec3 (1, 1, 0.5)) c2
 
 fbunch = (f * f * f + 0.5 *f * f + 0.6 * f)
-end_color = vec3 (fbunch, fbunch, fbunch) * c3 --TODO fix this last bit
+--end_color = vec3 (fbunch, fbunch, fbunch) * c3 --TODO fix this last bit
