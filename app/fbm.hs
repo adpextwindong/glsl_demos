74dd8af18e7f1aaa805317489ca6224fa511e5f1
Takumi Crary <tcrary@gmail.com>
2021-06-26 20:18:03 +0900

    Finish porting fbm example
    The constants get mangled during compilation so the output isn't exactly
    the same.
    
    For reference heres what this should look like http://glslsandbox.com/e#73620.0
    
    This version depends on these hotfix commits:
    
    https://github.com/adpextwindong/hylogen/commit/679815ad168554175ddbc3c914ceac132a469964
    https://github.com/adpextwindong/hylogen/commit/a29a1e656a7e574390ca234d03ce08bcbf58a48a

--- app/fbm.hs
+++ app/fbm.hs
@@ -53,40 +53,44 @@ octaves = 16
 
 --NOTE: Be careful of the num and fractional typeclasses forcing Vec n
 --TODO: Considering adding scalars to MulR and testing that
-p = ((2.0 * xy_ gl_FragCoord) - xy_ resolution) * vec2(r_minrz, r_minrz):: Vec2
-minrz = min_ (x_ resolution) (y_ resolution) :: Vec1
-r_minrz = (1 / minrz)
-time2 = 3.0 * time / 2.0 :: Vec1
-q = vec2 ( fbm octaves p, fbm octaves (p + 1.0))
-
-r = vec2 ( fbm octaves (p + 1.0 * q + vec2 (8.3, 2.8) + vec2 (0.15 * time2, 0.15 * time2)),
-       ( fbm octaves (p + 1.0 * q + vec2 (8.3, 2.8) + vec2 (0.126 * time2, 0.126 * time2))))
-
-f = fbm 16 (p + r)
---TODO UNLIFT THE CONTENT FOR THIS FUNCTION
 
 color :: Vec4
---color = vec4 (x_ end_color, y_ end_color, z_ end_color, 1.0)
-color = vec4 (noise p,
-               0.0,
-               0.0,
-               1.0)
+color = vec4(c4, 1.0)
+    where
+        minrz = min_ (x_ resolution) (y_ resolution) :: Vec1
+        r_minrz = (1 / minrz)
+        p = ((2.0 * xy_ gl_FragCoord) - xy_ resolution) * vec2(r_minrz, r_minrz):: Vec2
+
+        time2 = 3.0 * time / 2.0 :: Vec1
+
+        q = vec2 ( fbm octaves (p + mouse), fbm octaves (p + 1.0 + mouse))
+
+        x = 0.15 * time2
+        y = 0.126 * time2
+
+        r = vec2 (fbm octaves (p + (1.0 * q) + vec2 (1.7, 9.2) + vec2(x,x) - mouse),
+                  fbm octaves (p + (1.0 * q) + vec2 (8.3, 2.8) + vec2(y,y) + mouse))
+
+        f = fbm 16 (p + r + mouse)
+        --TODO UNLIFT THE CONTENT FOR THIS FUNCTION
 
 
---Ideally vec2 / vec1 should work but the fractional instance forces a -> a -> a
---Making it expect a vec1 on the right side of the /
---0.15 * time2 is another hiccup due to Num constraining type
+        --Ideally vec2 / vec1 should work but the fractional instance forces a -> a -> a
+        --Making it expect a vec1 on the right side of the /
+        --0.15 * time2 is another hiccup due to Num constraining type
 
+        c1 = mix (vec3 (-10, -10, 2))
+                 (vec3 (0.3667, 1.5939, 0.367) * vec3(mouse, 1.0))
+                 (clamp ((f*f) * 4.0) 0.0 1.0) + vec3(mouse, 1.0)
 
-t1 = clamp ((f * f) * 4.0) (0.0 :: Vec1) (1.0 :: Vec1)
---c1 = mix (vec3 (-10, -10, 2)) (vec3 (0.3667, 15939, 0.367) * (vec3 (x_ mouse, y_ mouse, 1.0)))
+        c2 = mix c1
+                 (vec3 (-10, -10, 0.5))
+                 (clamp (len q) 0.1 5.0)
 
-t2 = clamp (len q) 0.1 5.0
---c2 = mix t2 (vec3 (-10, -10, 0.5)) c1
---TODO double check mix ordering of arguments compared to original
+        c3 = mix c2
+                 (vec3 (1, 1, 0.5))
+                 (clamp (len (x_ r)) 0.0 1.0)
 
-t3 = clamp (len (x_ r)) 0.0 1.0
---c3 = mix t3 (vec3 (1, 1, 0.5)) c2
-
-fbunch = (f * f * f + 0.5 *f * f + 0.6 * f)
---end_color = vec3 (fbunch, fbunch, fbunch) * c3 --TODO fix this last bit
+        f2 = f * f * f + 0.5 * f * f + 0.6 * f
+        --TODO no instance for ToVec3 Vec1???
+        c4 = c3 * vec3(f2,f2,f2)
