0a9be98934e272ad84e26f5bcd91871e0b56b705
Takumi Crary <tcrary@gmail.com>
2021-06-26 16:38:46 +0900

    Narrow down bug to noise function for now
    http://gls3618.1lsandbox.com/e#7 for reference

--- app/fbm.hs
+++ app/fbm.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.VectorSpace
 import Hylogen
 import Hylogen.Expr
 import SandboxCanvasBoilerplate
@@ -28,37 +29,48 @@ fbm :: Int -> Vec2 -> Vec1
 fbm num_octaves pos = v
     where a = 0.5
           vinit = 0.5
-          shift = vec2(100.0, 100.0) + mouse
-          rot = mat22 ((cos 0.1) , (sin 0.5), (- sin 0.5), (cos 0.5))
-          fbmOctave (v,pos,a) = (v + a * noise pos, mul rot (pos * 2.0 + shift + mouse), a * 0.5)
+          shift = vec2(100.0, 100.0) + mouse :: Vec2
+          rot = mat22 ((cos 0.1) , (sin 0.5), (- sin 0.5), (cos 0.5)) :: M22
+          fbmOctave (v,pos,a) = (v + a * noise pos, ((rot *^* pos) * 2.0) + shift + mouse, a * 0.5)
           xs = iterate fbmOctave (vinit, pos, a) --I hope this works
           (v, _, _) = xs !! num_octaves
 
 octaves = 16
 
+--NOTE: Be careful of the num and fractional typeclasses forcing Vec n
+--TODO: Considering adding scalars to MulR and testing that
+p = ((2.0 * xy_ gl_FragCoord) - xy_ resolution) * vec2(r_minrz, r_minrz):: Vec2
+minrz = min_ (x_ resolution) (y_ resolution) :: Vec1
+r_minrz = (1 / minrz)
+time2 = 3.0 * time / 2.0 :: Vec1
+q = vec2 ( fbm octaves p, fbm octaves (p + 1.0))
+
+r = vec2 ( fbm octaves (p + 1.0 * q + vec2 (8.3, 2.8) + vec2 (0.15 * time2, 0.15 * time2)),
+       ( fbm octaves (p + 1.0 * q + vec2 (8.3, 2.8) + vec2 (0.126 * time2, 0.126 * time2))))
+
+f = fbm 16 (p + r)
+--TODO UNLIFT THE CONTENT FOR THIS FUNCTION
 color :: Vec4
-color = vec4 (x_ end_color, y_ end_color, z_ end_color, 1.0)
-    where p = ((xy_ gl_FragCoord * 2.0 * xy_ resolution) / vec2 (minrz, minrz)) - mouse / 100.1 :: Vec2
-          --Ideally vec2 / vec1 should work but the fractional instance forces a -> a -> a
-          --Making it expect a vec1 on the right side of the /
-          minrz = min_ (x_ resolution) (y_ resolution)
-          time2 = 3.0 * time / 2.0 :: Vec1
-          q = vec2 ( fbm octaves (p + mouse) , fbm octaves (p + (1.0 + mouse)))
-          --0.15 * time2 is another hiccup due to Num constraining type
-          r = vec2 ( fbm octaves (p + 1.0 * q + vec2 (8.3, 2.8) + vec2 (0.15 * time2, 0.15 * time2) - mouse),
-                   ( fbm octaves (p + 1.0 * q + vec2 (8.3, 2.8) + vec2 (0.126 * time2, 0.126 * time2) + mouse)))
+--color = vec4 (x_ end_color, y_ end_color, z_ end_color, 1.0)
+color = vec4 (noise (p + 1.0),
+             (noise (p + 1.0 * q + vec2 (8.3, 2.8) + vec2 (0.15 * time2, 0.15 * time2))),
+             (noise (p + 1.0 * q + vec2 (8.3, 2.8) + vec2 (0.126 * time2, 0.126 * time2))),
+               1.0)
 
-          f = fbm 16 (p + r + mouse)
+--Ideally vec2 / vec1 should work but the fractional instance forces a -> a -> a
+--Making it expect a vec1 on the right side of the /
+--0.15 * time2 is another hiccup due to Num constraining type
 
-          t1 = clamp ((f * f) * 4.0) (0.0 :: Vec1) (1.0 :: Vec1)
-          c1 = mix t1 (vec3 (-10, -10, 2)) (vec3 (0.3667, 15939, 0.367) * (vec3 (x_ mouse, y_ mouse, 1.0)))
 
-          t2 = clamp (len q) 0.1 5.0
-          c2 = mix t2 (vec3 (-10, -10, 0.5)) c1
-          --TODO double check mix ordering of arguments compared to original
+t1 = clamp ((f * f) * 4.0) (0.0 :: Vec1) (1.0 :: Vec1)
+c1 = mix t1 (vec3 (-10, -10, 2)) (vec3 (0.3667, 15939, 0.367) * (vec3 (x_ mouse, y_ mouse, 1.0)))
 
-          t3 = clamp (len (x_ r)) 0.0 1.0
-          c3 = mix t3 (vec3 (1, 1, 0.5)) c2
+t2 = clamp (len q) 0.1 5.0
+c2 = mix t2 (vec3 (-10, -10, 0.5)) c1
+--TODO double check mix ordering of arguments compared to original
 
-          fbunch = (f * f * f + 0.5 *f * f + 0.6 * f)
-          end_color = vec3 (fbunch, fbunch, fbunch) * c3 --TODO fix this last bit
+t3 = clamp (len (x_ r)) 0.0 1.0
+c3 = mix t3 (vec3 (1, 1, 0.5)) c2
+
+fbunch = (f * f * f + 0.5 *f * f + 0.6 * f)
+end_color = vec3 (fbunch, fbunch, fbunch) * c3 --TODO fix this last bit
